9603ea0320824249ebf41c5829c1934a779cbf9e
alexxtomas <atomasllimos@gmail.com>
2023-07-03 12:32:52 +0200

    feat: Import src entry point

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
 import './style.css'
-import './src/components'
-
-document.querySelector('#error')?.addEventListener('click', () => {
-  const input = document.querySelector('dashboard-input')
-
-  input!.innerHTML = `<span slot="validation-error">Error</span>`
-})
+import './src/index.ts'

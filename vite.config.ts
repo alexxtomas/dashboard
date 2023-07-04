2b839984302cfc26567a59b4e2c6394d647f4df9
alexxtomas <atomasllimos@gmail.com>
2023-07-04 15:55:29 +0200

    feat: Set up vitest with happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  },
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
